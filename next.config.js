fef99acee89f989db2fda5994bb841fc80de5045
Łukasz Szymczyk <lukasz.szymczyk.94@gmail.com>
2023-05-21 13:57:10 +0200

    env variables

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    FIREBASE_API_KEY: "http://mysite.com",
-    nextVar: "MyVar",
-  },
 }
 
 module.exports = nextConfig
